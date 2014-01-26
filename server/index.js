4484ef5f070ac41514e3d21ef0cba5e6541c4bb9
Michael Carius <michael.a.carius@gmail.com>
2014-01-25 16:29:40 -0800

    Add ready message to server.

--- server/index.js
+++ server/index.js
@@ -4,30 +4,15 @@ var peers = {};
 var waiting = null;
 
 io.sockets.on("connection", function(socket) {
-    // join - matchmaking
-    socket.on("join", function() {
-        if(waiting !== null) {
-            peers[socket] = waiting;
-            peers[waiting] = socket;
-            socket.emit("ready");
-            waiting.emit("ready");
-            waiting = null;
-        }
+    // matchmaking
+    if(waiting !== null) {
+        peers[waiting] = socket;
+        peers[socket] = waiting;
+        socket.emit("ready");
+        waiting.emit("ready");
+        waiting = null;
+    }
 
-        else {
-            socket.emit("searching");
-            waiting = socket;
-        }
-    });
-
-    // disconnect - exit game instance
-    socket.on("disconnect", function() {
-        var peer = peers[socket];
-        peer.emit("quit");
-
-        peers[socket] = null;
-        delete peers[socket];
-        peers[peer] = null;
-        delete peers[peer];
-    });
+    else
+        waiting = socket;
 });
